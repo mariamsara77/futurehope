6e53fcf5baee10be6ff21f2701ecbf1c32b9259f
mariamsara77 <118017103+mariamsara77@users.noreply.github.com>
2026-09-22 23:15:25 +0600

    fix: allow production frontend CORS

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'https://futurehope.totthobox.com',
         'https://future-hope-and-humanity-foundation.vercel.app',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
