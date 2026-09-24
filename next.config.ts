b3ac8044b3a30a7f41f6b8cf9ac92329625c53d7
mariamsara77 <118017103+mariamsara77@users.noreply.github.com>
2026-09-24 23:14:06 +0600

    Allow optimized Futurehope work images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "futurehope.totthobox.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
