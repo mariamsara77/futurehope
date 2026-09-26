13528c8160d7763f1b946b390c196ddc05728af2
mariamsara77 <118017103+mariamsara77@users.noreply.github.com>
2026-09-26 15:52:21 +0600

    Add balanced authentication rate limits

--- routes/api.php
+++ routes/api.php
@@ -62,9 +62,11 @@ Route::post('/works', [WorkController::class, 'store'])
 
 Route::prefix('auth')->group(function () {
 
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/register', [AuthController::class, 'register'])
+        ->middleware('throttle:10,1');
 
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])
+        ->middleware('throttle:10,1');
 
     /*
     |--------------------------------------------------------------------------
